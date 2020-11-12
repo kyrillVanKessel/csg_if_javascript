b2dc4fb19f771f3faf3a2ceb804ae1b176573da9
kyrillVanKessel <152209@csgaugustinus.nl>
2020-11-12 11:48:56 +0000

    sjkfl;a

--- JS/H1/H1O02.js
+++ JS/H1/H1O02.js
@@ -1,12 +1,26 @@
 function setup() {
   canvas = createCanvas(450,450);
-  background('orange');
+  background('darkred');
   canvas.parent('processing');
   noLoop();
 }
 
 function draw() {
   noStroke();
+  fill('pink');
+  triangle(width,0,width,height,0,height/2);
+  
+  noStroke();
+  fill('purple');
+  triangle(0,0,0,height,width,height/2);
+
+  strokeWeight(5);
+  stroke('pink');
   fill('darkred');
-  triangle(225,0,0,450,450,450);
+  beginShape();
+  vertex(225,115);
+  vertex(300,225);
+  vertex(225,335);
+  vertex(0,225);
+  endShape(CLOSE);
 }
