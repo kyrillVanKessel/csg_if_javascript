7db436f0995e3d2363d5aa9e85bd91c9a9e78044
kyrillVanKessel <152209@csgaugustinus.nl>
2020-11-19 08:54:25 +0000

    changes

--- JS/H1/H1O03.js
+++ JS/H1/H1O03.js
@@ -3,16 +3,13 @@ function setup() {
   background('white');
   canvas.parent('processing');
   noLoop();
-  // colorMode(RGB,255,255,255,1);  
+  colorMode(RGB,255,255,255,1);  
 }
 
 function draw() {
   noStroke();
-  // fill(0,0,255,0.3);
-  fill('red');
+  fill(0,0,255,0.3);
   rect(0,150,450,150);
-  fill(0, 128, 0);
   rect(0,0,150,450);
-  fill('blue');  
   rect(75,0,375,450);
 }
