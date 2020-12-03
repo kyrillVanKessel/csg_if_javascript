d82b803ff911d503b6e2d6c2710ec60dd0f027f9
kyrillVanKessel <152209@csgaugustinus.nl>
2020-12-03 11:59:38 +0000

    asdf

--- JS/H1/H1O16.js
+++ JS/H1/H1O16.js
@@ -1,5 +1,6 @@
 var diameter = 300;
-
+var gorb = -diameter;
+var borb = height + diameter;
 function setup() {
   canvas = createCanvas(450,450);
   canvas.parent('processing');
@@ -11,4 +12,10 @@ function draw() {
   background(255,255,255,1);
   fill(255,0,0,0.5);
   ellipse(width / 2,height / 2,diameter);
+  fill(0,255,0,0.5);
+  ellipse(width/2, gorb, diameter)
+  fill(0,0,255,0.5);
+  ellipse(width/2,borb,diameter);
+  gorb++;
+  borb--;
 }
