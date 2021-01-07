7e2681bd5e41230627a2220fb17e7b0fcf7f9518
kyrillVanKessel <152209@csgaugustinus.nl>
2021-01-07 08:35:11 +0000

    lol

--- JS/test.js
+++ JS/test.js
@@ -1,15 +1,23 @@
+var g = 75;
+var n = 5;
 function setup() {
   canvas = createCanvas(450,450);
   background('silver');
   canvas.parent('processing');
-  //noLoop();
+  noLoop();
 }
 
 function draw() {
   noStroke();
   translate(0,height/2);
-  for(var x=0; x<5; x+=1) {
-      rect(0,0,75,75);
-      translate
+  for(var x=0; x<n; x+=1) {
+      rect(0,0,g,g);
+      if(x==1){
+          fill('blue');
+      }
+      else {
+      fill('red');
+      }
+      translate(g+(width+g/4-n*g)/n,0);
   }
 }
